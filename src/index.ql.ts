c4f083361787854bf225243784035b8a6b024b67
catlair <catlair@qq.com>
2023-02-20 20:48:44 +0800

    chore: ql commond options

--- src/index.ql.ts
+++ src/index.ql.ts
@@ -1,23 +1,19 @@
+import type { ConfigArray } from './types/config';
 import { config, runTask, waitForArgs } from './util';
 import { getArg, isArg } from './utils/args';
 
 process.env.IS_QING_LONG = 'true';
 
 (async () => {
-  if (isArg('config')) {
-    await waitForArgs();
-    const configs = await config();
-    if (!configs) {
-      return;
-    }
-    const taskArg = getArg('task');
-    if (taskArg) {
-      return await runTask(configs, './bin/inputTask', taskArg);
-    }
-    return await runTask(configs);
-  }
+  let configs: ConfigArray | undefined;
   await waitForArgs();
-  const { getConfig } = await import('./config/utils');
-  const configs = getConfig(true);
+  if (isArg('config')) {
+    configs = await config();
+  }
+  const taskArg = getArg('task');
+  if (taskArg) {
+    return await runTask(configs, './bin/inputTask', taskArg);
+  }
+  if (!configs) return;
   return await runTask(configs);
 })();
