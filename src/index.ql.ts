99f56905629f3c8d197e3bb5dc380ec8a086abfb
catlair <catlair@qq.com>
2023-08-24 04:54:38 -1000

    chore: qinglong task

--- src/index.ql.ts
+++ src/index.ql.ts
@@ -1,23 +1,29 @@
-import { config, runTask, waitForArgs } from './util';
+import type { ConfigArray } from './types';
+import { config, getConfigByItem, runTask, waitForArgs } from './util';
 import { getArg, isArg } from './utils/args';
 
 process.env.IS_QING_LONG = 'true';
 
 (async () => {
+  let configs: ConfigArray | undefined;
+  await waitForArgs();
   if (isArg('config')) {
-    await waitForArgs();
-    const configs = await config();
+    configs = await config();
     if (!configs) {
       return;
     }
-    const taskArg = getArg('task');
-    if (taskArg) {
-      return await runTask(configs, './bin/inputTask', taskArg);
-    }
     return await runTask(configs);
+  } else {
+    const { getConfig } = await import('./config/utils');
+    configs = getConfig(true);
+    const itemArg = getArg('item');
+    if (itemArg) {
+      configs = getConfigByItem(configs, itemArg);
+    }
   }
-  await waitForArgs();
-  const { getConfig } = await import('./config/utils');
-  const configs = getConfig(true);
+  const taskArg = getArg('task');
+  if (taskArg) {
+    return await runTask(configs, './bin/inputTask', taskArg);
+  }
   return await runTask(configs);
 })();
