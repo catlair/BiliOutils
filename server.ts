b10046a6b9834c069e8734db90c1648a136b636a
catlair <catlair@qq.com>
2024-01-06 22:40:29 +0800

    wip: webHeartbeat

--- server.ts
+++
@@ -1,7 +0,0 @@
-import { createServer } from 'http';
-
-createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write(req.url);
-  res.end();
-}).listen(8080);
--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -4,5 +4,7 @@ import type { RequestOptions } from './request';
 declare module '@catlair/node-got' {
   export interface RequestOptions {
     withBiliCookie?: boolean;
+    withAppSign?: boolean;
+    withBiliCsrf?: boolean;
   }
 }
