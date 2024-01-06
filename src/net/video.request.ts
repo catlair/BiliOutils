61e2669af7445012412c129e2a3aa34bbbd3f68f
catlair <catlair@qq.com>
2024-01-06 22:39:39 +0800

    wip: webHeartbeat

--- src/net/video.request.ts
+++ src/net/video.request.ts
@@ -3,12 +3,12 @@ import type {
   ShareAddDto,
   RegionRankingVideosDto,
   DonatedCoinsForVideoDto,
-  HeartbeatDto,
   RecommendVideoDto,
   BangumiFollowDto,
 } from '../dto/video.dto';
 import { biliApi } from './api';
 import { TaskConfig } from '../config';
+import { isString } from '@/utils/is';
 
 /**
  * 分享视频
@@ -57,80 +57,126 @@ export function donatedCoinsForVideo(aid: number): Promise<DonatedCoinsForVideoD
 
 /**
  * 上传视频已经观看时间
- * @param aid av号
+ * @param id av号/bv号
  * @param playedTime 观看时间
  */
-export function uploadVideoHeartbeat(
-  aid: number | string,
-  playedTime: number,
-): Promise<HeartbeatDto> {
-  return biliApi.post('x/click-interface/web/heartbeat', {
-    aid,
-    dt: 2,
-    type: 3,
-    sub_type: 0,
+export function uploadVideoHeartbeat(id: number | string, playedTime: number) {
+  const options: HeartbeatParams = {
     start_ts: Math.floor(Date.now() / 1000) - playedTime,
     played_time: playedTime,
-    real_played_time: playedTime + 1,
-    refer_url: 'https://www.bilibili.com/',
-    csrf: TaskConfig.BILIJCT,
-  });
-}
-
-interface HeartbeatParams {
-  start_ts: number;
-  aid?: number;
-  cid?: number;
-  type?: number;
-  sub_type?: number;
-  dt?: number;
-  play_type?: number;
-  realtime?: number;
-  played_time?: number;
-  real_played_time?: number;
-  refer_url?: string;
-  sid?: number;
-  epid?: number;
+    type: 3,
+    sub_type: 0,
+  };
+  if (isString(id) && id.toLowerCase().startsWith('bv')) {
+    options.bvid = id;
+  } else {
+    options.aid = Number(id);
+  }
+  return webHeartbeat(options);
 }
 
 /**
  * 视频观看心跳
  */
-export function videoHeartbeat({
-  start_ts,
-  aid = 243332804,
-  cid = 209599683,
-  type = 4,
-  sub_type = 5,
-  dt = 2,
-  play_type = 0,
-  realtime = 0,
-  played_time = 10,
-  real_played_time = 0,
-  refer_url = '',
-  sid = 33622,
-  epid = 327107,
-}: HeartbeatParams) {
-  return biliApi.post<Omit<ApiBaseProp, 'data'>>('x/click-interface/web/heartbeat', {
-    start_ts,
-    mid: TaskConfig.USERID,
-    aid,
-    cid,
-    type,
-    sub_type,
-    dt,
-    play_type,
-    realtime,
-    played_time,
-    real_played_time,
-    refer_url,
+export function videoHeartbeat(params: HeartbeatParams) {
+  return webHeartbeat({
+    aid: 243332804,
+    cid: 209599683,
+    type: 4,
+    sub_type: 5,
+    dt: 2,
+    play_type: 0,
+    realtime: 0,
+    played_time: 10,
+    real_played_time: 0,
+    sid: 33622,
+    epid: 327107,
+    ...params,
+  });
+}
+
+interface HeartbeatParams {
+  mid?: number;
+  /**开始播放时刻  */
+  start_ts: number;
+  /** 稿件aid 与 bvid 二选一 */
+  aid?: number;
+  /** 稿件bvid 与 aid 二选一 */
+  bvid?: string;
+  /** 稿件cid 识别分P */
+  cid?: number;
+  /** 视频类型  3：投稿视频 4：剧集 10：课程 */
+  type?: number;
+  /** 0：无 1：番剧 2：电影 3：纪录片 4：国创 5：电视剧 7：综艺 */
+  sub_type?: number;
+  /** ？ 2 */
+  dt?: number;
+  /** 0：播放中 1：开始播放 2：暂停 3：继续播放 4：播放结束  */
+  play_type?: number;
+  /** 总计播放时间 单位 s */
+  realtime?: number;
+  /** 播放进度 单位 s，-1 为播放结束 */
+  played_time?: number;
+  /** 实际总播放时间（非单次）??? */
+  real_played_time?: number;
+  refer_url?: string;
+  /** 番剧sid */
+  sid?: number;
+  /** 番剧epid */
+  epid?: number;
+  session?: string;
+  /** {"player_version":"4.7.12"} */
+  extra?: string;
+  csrf?: string;
+  /** 333.788.0.0 */
+  spmid?: string;
+  /** 333.1007.tianma.1-2-2.click 333.788.recommend_more_video.0 */
+  from_spmid?: string;
+  /** 0 */
+  outer?: number;
+  /** 进度条时间，这不和 played_time 一样吗 */
+  last_play_progress_time?: number;
+  /** 视频长度（视频播放结束后是 长度 - 1，怪） */
+  video_duration?: number;
+  /** 最大已播放进度（该值最大值会随 video_duration 变化，video_duration - 1 时它也会）  ？ */
+  max_play_progress_time?: number;
+  quality?: number;
+  bsource?: string;
+}
+
+/**
+ * 视频观看心跳
+ */
+export function webHeartbeat(params: HeartbeatParams) {
+  const options: HeartbeatParams = {
+    dt: 2,
+    play_type: 0,
+    realtime: 0,
+    played_time: 10,
+    real_played_time: 0,
+    refer_url: '',
     bsource: '',
-    sid,
-    epid,
     spmid: '666.25',
     from_spmid: '..0.0',
+    mid: TaskConfig.USERID,
     csrf: TaskConfig.BILIJCT,
-  });
+    ...params,
+  };
+  if (options.aid && options.bvid) {
+    Reflect.deleteProperty(options, 'bvid');
+  }
+  return biliApi.post<Omit<ApiBaseProp, 'data'>>(
+    `x/click-interface/web/heartbeat?w_start_ts=${options.start_ts}&w_mid=${options.mid}&w_${
+      options.aid ? `aid=${options.aid}` : `bvid=${options.bvid}`
+    }&w_dt=${options.dt}&w_realtime=${options.realtime}&w_played_time=${
+      options.played_time
+    }&w_real_played_time=${options.real_played_time}&w_video_duration=${
+      options.video_duration
+    }&w_last_play_progress_time=${
+      options.last_play_progress_time || options.played_time
+    }&web_location=1315873`,
+    options,
+  );
 }
 
 /**
--- src/utils/got/config.ts
+++ src/utils/got/config.ts
@@ -1,5 +1,9 @@
-import type { VGotOptions } from '@catlair/node-got';
+import type { VGotOptions, NormalizedOptions } from '@catlair/node-got';
 import { defaultHeaders } from '@/constant/biliUri';
+
+type HooksOptions = NormalizedOptions & VGotOptions;
+
+const wbiArrays = ['/x/click-interface/web/heartbeat'];
 
 export function getOptions(): VGotOptions {
   return {
@@ -18,15 +22,22 @@ export function getOptions(): VGotOptions {
       ignoreCancelToken: true,
       // 是否携带 bili cookie
       withBiliCookie: true,
+      withAppSign: false,
+      withBiliCsrf: false,
     },
     throwHttpErrors: false,
     hooks: {
       beforeRequest: [
-        async options => {
-          const { TaskConfig } = await import('@/config');
+        async (options: HooksOptions) => {
+          // const { TaskConfig } = await import('@/config');
           const { getWbiQuery } = await import('@/service/sgin.service');
           const searchParams = new URLSearchParams(options.url.search);
-          searchParams.set('csrf', TaskConfig.BILIJCT);
+          // if (options.requestOptions?.withBiliCsrf) {
+          //   searchParams.set('csrf', TaskConfig.BILIJCT);
+          // }
+          // if (options.requestOptions?.withAppSign) {
+          //   appSign(searchParams);
+          // }
           if (['x/space/wbi/arc/search'].includes(options.url.pathname)) {
             searchParams.set('dm_img_list', '[]');
             searchParams.set('dm_img_str', 'V2ViR0wgMS4wIChPcGVuR0wgRVMgMi4wIENocm9taXVtKQ');
@@ -35,7 +46,10 @@ export function getOptions(): VGotOptions {
               'QU5HTEUgKEludGVsLCBJbnRlbChSKSBVSEQgR3JhcGhpY3MgNjMwICgweDAwMDAzRTkyKSBEaXJlY3QzRDExIHZzXzVfMCBwc181XzAsIEQzRDExKUdvb2dsZSBJbmMuIChJbnRlbC',
             );
           }
-          if (options.url.pathname.includes('/wbi')) {
+          if (
+            options.url.pathname.includes('/wbi') ||
+            wbiArrays.some(url => options.url.pathname === url)
+          ) {
             await getWbiQuery(searchParams);
           }
           options.url.search = searchParams.toString();
