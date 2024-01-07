81feeeb1bfde263babe1e55d6913ecd1a49747d9
catlair <catlair@qq.com>
2024-01-07 20:44:53 +0800

    chore: remove lotteryScan

--- src/task/lotteryScan/utils.ts
+++
@@ -1,8 +0,0 @@
-const enquoteBigNumber = (jsonString: string) =>
-  jsonString.replace(/"business_id":(\d+)/, '"business_id":"$1"');
-
-export function parseJson(jsonString: string) {
-  return JSON.parse(enquoteBigNumber(jsonString), (key, value) =>
-    key === 'business_id' ? BigInt(value) : value,
-  );
-}
